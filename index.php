61fa29c76ddcaea491c6b8beebfa5a913ca917d7
PierluigiTornesi <pierluigi.tornesi02@gmail.com>
2023-12-13 17:11:48 +0100

    add: milestone 1

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
+<?php 
+
+    $email = '';
+    if(isset($_GET['email'])){
+        $email = $_GET['email'];
+        $flag = false;
+
+        if((strpos($email,'@')) &&  (strpos($email,'.'))){
+            $flag = true;
+        }
+    }
+    var_dump($email);
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,6 +20,25 @@
     <title>PHP Iscrizione Newsletter</title>
 </head>
 <body>
+    <form action="index.php" method="get">
+        <label for="email">Inserisci la tua email</label>
+        <input type="text" id="email" name="email" placeholder="email@example.com">
+
+        <button type="submit">Invia</button>
+    </form>
+
+    <?php 
     
+    if($email === ''){
+        echo '';
+    }else{
+        if($flag){
+            echo 'La tua email ' . $email . ' é valida';
+        }else{
+            echo 'La tua email ' . $email . ' non é valida';
+        }
+    }
+
+    ?>
 </body>
 </html>
